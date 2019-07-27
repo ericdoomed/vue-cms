30578a64e108a06828d56c590d40266e93bc618f
ericdoomed <d1464163639@gmail.com>
2019-07-27 17:49:16 +0800

    init the project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+console.log('OK')
+
+import Vue from 'vue'
+
+import './lib/mui/css/mui.min.css'
+
+import { Header } from 'mint-ui'
+Vue.component(Header.name,Header)
+
+import app from './App.vue'
+
+var vm = new Vue({
+    el:'#app',
+    render: c => c(app)
+})
